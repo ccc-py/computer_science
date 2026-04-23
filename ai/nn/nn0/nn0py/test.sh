82aa7a8b271dddc87bfb5ec22f94747145fac699
ccckmit <ccckmit@gmail.com>
2026-04-23 08:16:57 +0800

    add nn0py/*.md

--- ai/nn/nn0/nn0py/test.sh
+++ ai/nn/nn0/nn0py/test.sh
@@ -6,6 +6,6 @@ python examples/ex2-linear.py
 python examples/ex3-xor.py
 python examples/ex4-classify.py
 python examples/ex5-charpredicate.py
-python examples/ex5b.py
-# python examples/test_gpt0.py ../_data/corpus/chinese.txt
-python examples/test_cnn0.py ../_data/corpus/chinese.txt
+# python examples/ex5b.py
+python examples/gpt0demo.py ../_data/corpus/tw.txt
+# python examples/test_cnn0.py ../_data/corpus/chinese.txt
